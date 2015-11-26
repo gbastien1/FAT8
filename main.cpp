c7a8ce07cc066b319f61d09cffdebdce45331241
gbastien1 <gabrielle.bastien1@gmail.com>
2015-11-26 18:24:41 -0500

    update OS class

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
 using namespace std;
 
 class HardDrive {
@@ -8,15 +11,29 @@ class HardDrive {
 class OS {
 
 	HardDrive *hd;
+	int FAT [256];
+	vector< vector< int > > fileBlock;
 
 public:
 	OS() { hd = new HardDrive(); }
 	~OS() { delete hd; }
 
-	void read();
-	void write();
-	void deleteEOF();
-	void printHD();
+	void read(string nomFichier, int position, int nombreCaracteres, string &tampLecture) {
+		
+	}
+
+	void write(String nomFichier, int position, int nombreCaracteres, String tampLecture) {
+		ofstream file(nomFichier);
+
+	}
+
+	void deleteEOF(String nomFichier, int position) {
+
+	}
+
+	void printHD() {
+
+	}
 };
 
 int main(void) {
