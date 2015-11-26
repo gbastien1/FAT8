4a3bc69d003e9f2568078e57341d22560f2f60a9
gbastien1 <gabrielle.bastien1@gmail.com>
2015-11-26 16:51:29 -0500

    debut de classe OS

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
 
+class HardDrive {
+
+};
+
+class OS {
+
+	HardDrive *hd;
+
+public:
+	OS() { hd = new HardDrive(); }
+	~OS() { delete hd; }
+
+	void read();
+	void write();
+	void deleteEOF();
+	void printHD();
+};
+
 int main(void) {
 	
 	return 0;
