34d98faaf9b0aca2ee9b308c915ad2a38bb87ad6
Qc-Leblond <antoine.brouillette@hotmail.com>
2015-12-03 18:50:33 -0500

    Working on Delete

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <map>
+#include <cmath>
 using namespace std;
 
 #define BLOCK_SIZE 64
@@ -36,7 +38,7 @@ public:
 			file >> temp;
 			memory += temp;
 		}
-		memory += FillWithWhiteSpace(BLOCK_SIZE - memory.length);
+		memory += FillWithWhiteSpace(BLOCK_SIZE - memory.length());
 
 		for (int i=0; i < memory.length(); i++) {
 			if (i > BLOCK_SIZE * numeroBlock) {
@@ -84,13 +86,29 @@ public:
 		file.close();
 		getFile.close();
 	}
+
+	/*  
+	 * Get the first available empty memory space.
+	 */
+	int GetFirstAvailableMemorySpace() {
+		ifstream file(DISK_FILENAME);
+
+		for (int i=1; 1; i++) {
+			if (i == BLOCK_COUNT - 1) return -1;
+			if (file.eof()) return i;
+			string out;
+			file >> out;
+			if (out[0] == ' ') return i;
+		}
+
+		file.close();
+	}
 };
 
 class OS {
 
 	HardDrive *hd;
 	int FAT [256];
-	vector<vector< int >> fileBlock;
 	map<string, int> files; //corresponding filename and fileID
 	static int fileID;
 	
@@ -111,7 +129,16 @@ public:
 	}
 
 	void deleteEOF(string nomFichier, int position) {
+		int startBlock = floor(position / BLOCK_SIZE); //The block to start with
+		int index = files[nomFichier];
 
+		for (int i=0; i < startBlock; i++) {
+			index = FAT[index];
+		}
+
+		while (FAT[index] != 0) {
+
+		}
 	}
 
 	void printHD() {
